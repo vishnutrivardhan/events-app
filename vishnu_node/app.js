327045046db9ae0c963826bbc469d5546b5dc340
Vishnu <vishnutrivardhan@gmail.com>
2022-12-15 17:30:29 -0500

    changed port to 80

--- vishnu_node/app.js
+++ vishnu_node/app.js
@@ -57,7 +57,7 @@ const server = http.createServer((req, res) => {
         });
     }
 });
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => console.log(`Server is running successfully and the port number is ${PORT}`))
 
 
